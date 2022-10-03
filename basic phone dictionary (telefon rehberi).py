f63749fc55b85033ed96f0f02d75e55a42fe99ac
Atakan Özcan <103129507+Owayodata@users.noreply.github.com>
2022-10-03 21:14:20 +0300

    Add files via upload

---
+++ basic phone dictionary (telefon rehberi).py
@@ -0,0 +1,11 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> isimtel = {'Atakan':'+90 505 xxx xxxx', 'Uğur':'+90 507 xxx xxxx','Yağmur':'+90 555 xxx xxxx'}
+>>> isimtel
+{'Atakan': '+90 505 xxx xxxx', 'Uğur': '+90 507 xxx xxxx', 'Yağmur': '+90 555 xxx xxxx'}
+>>> isimtel['Atakan']
+'+90 505 xxx xxxx'
+>>> isimtel['Uğur']
+'+90 507 xxx xxxx'
+>>> isimtel['Yağmur']
+'+90 555 xxx xxxx'
